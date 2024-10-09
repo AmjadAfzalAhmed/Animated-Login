aad3c15676862f50c87bb61a0ddc51e2a7acf072
AmjadAfzalAhmed <abhutto2006@gmail.com>
2024-10-09 19:35:40 +0500

    all done

---
+++ script.js
@@ -0,0 +1,12 @@
+const signInBtnLink = document.querySelector('.sign-in-link');
+const signUpBtnLink = document.querySelector('.sign-up-link');
+
+const wrapper = document.querySelector('.wrapper');
+
+signUpBtnLink.addEventListener('click',()=>{
+    wrapper.classList.toggle('active');
+})
+
+signInBtnLink.addEventListener('click',()=>{
+    wrapper.classList.toggle('active');
+})
